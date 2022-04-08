806103b6b11cee32f9894d92bb37d75bb6d2198f
younesDVE <younessakkioui99@gmail.com>
2022-04-08 22:37:49 +0000

    gestion panne

--- src/main/java/fstf/doa/DepartmentDAO.java
+++ src/main/java/fstf/doa/DepartmentDAO.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DepartmentDAO extends CrudRepository<Department,String> {
+    Department findDepartmentByName(String name);
 
 }
